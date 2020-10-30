92d8d2e44b27af30da356e4be25abe7f33ed040a
Sanjoy layek <sanjoy3741geo@gmail.com>
2020-10-30 05:50:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,161 @@
+from copy import deepcopy
+# Isolation Game
+xlim,ylim = 3,2
+
+class GameState:
+
+  def __init__(self):
+      #GameBoard
+      self._board = [[-1] * xlim for _ in range(ylim)]
+      self._board[-1][-1] = 1 
+      #Current Player 
+      self._parity = 0
+
+      #Current Pointion
+      self._player_location = [None,None]
+      print(self._board)
+
+  def get_legal_moves(self):
+    legalMoves= []
+    if self._player_location[0] == None:
+      #all are posible moves
+      i,j = 0,0
+
+      while( i>= 0 and i<ylim):
+        while( j>=0 and j < xlim ):
+          # print(i,j)
+          if(i>=0 and j < xlim and i<ylim and j >= 0 and self._board[i][j] == -1):
+            legalMoves.append((i,j))
+          else:
+            break
+          j += 1
+        j = 0
+        i += 1
+
+        
+    else:
+      #from that position look in all direction
+      posi = self._player_location[0][0]
+      posj= self._player_location[0][1]
+      i,j = posi,posj
+
+      #up
+      while(i>=0):
+        i -= 1
+        if(i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          print("up",i,j)
+          legalMoves.append((i,j))
+        else:
+          break
+                  
+      i,j = posi,posj
+
+      #down
+      while( i>=0 and j < xlim and i<ylim and j >= 0  ):
+        i += 1
+
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          # print("Down",i,j)
+          legalMoves.append((i,j))
+        else:
+          break
+
+      i,j = posi,posj
+
+      #left
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        j -= 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          legalMoves.append((i,j))
+        else:
+          break
+
+      i,j = posi,posj
+
+      #Right
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        j += 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j]== -1):
+          # print("R",i,j)
+          legalMoves.append((i,j))
+        else:
+          break
+
+      i,j = posi,posj
+
+      #Diagonal S-E
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        i += 1
+        j += 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          legalMoves.append((i,j))
+        else:
+          break
+      i,j = posi,posj
+
+      #Diagonal S-w
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        i -= 1
+        j += 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          legalMoves.append((i,j))
+        else:
+          break
+
+      i,j = posi,posj
+
+      #Diagonal N-E
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        i += 1
+        j -= 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          legalMoves.append((i,j))
+        else:
+          break
+
+      i,j = posi,posj
+
+      #Diagonal N-w
+      while( i>=0 and j < xlim and i<ylim and j >= 0 ):
+        i -= 1
+        j -= 1
+        if( i>=0 and j < xlim and i<ylim and j >= 0  and self._board[i][j] == -1):
+          legalMoves.append((i,j))
+        else:
+          break
+    return legalMoves
+
+  def forecast_move(self, move):
+      """ Return a new board object with the specified move
+      applied to the current game state.
+      
+      Parameters
+      ----------
+      move: tuple
+          The target position for the active player's next move
+          (e.g., (0, 0) if the active player will move to the
+          top-left corner of the board)
+      """
+      # TODO: implement this function!
+      if move not in self.get_legal_moves():
+        raise RuntimeError("Attempted forecast of illegal move")
+      newBoard = deepcopy(self)
+      newBoard._board[move[0]][move[1]] = 1
+      newBoard._player_location[self._parity] = move
+      newBoard._parity ^= 1
+      return newBoard
+
+  def _get_blank_spaces(self):
+    """ Return a list of blank spaces on the board."""
+    return [(x, y) for y in range(ylim) for x in range(xlim)
+            if self._board[x][y] == -1]
+
+
+if __name__ == "__main__":
+    # This code is only executed if "gameagent.py" is the run
+    # as a script (i.e., it is not run if "gameagent.py" is
+    # imported as a module)
+    emptyState = GameState()
+    emptyState.forecast_move((0,1))
+    # create an instance of the object
+    
